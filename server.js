ef5d3a9f7946d641be4964dcb000952cde428b35
Akndju <davidd.akindoju@gmail.com>
2021-04-20 14:25:52 +0100

    Created sign in route and ability to send wrong credentials to database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const knex = require('knex');
-
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 5000;
 
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const database = knex({
+const sql = knex({
   client: 'mysql',
   connection: {
     host: '127.0.0.1',
@@ -21,21 +21,70 @@ const database = knex({
   },
 });
 
-app.post('/register', (req, res) => {
+app.post('/signin', (req, res) => {
   const { email, password } = req.body;
-  database('users')
-    .insert({
-      email: email,
-      password: password,
+
+  sql
+    .select('email', 'password')
+    .from('signin')
+    .where('email', '=', email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].password);
+      if (isValid) {
+        return sql
+          .select('*')
+          .from('users')
+          .where('email', '=', email)
+          .then((user) => res.json(user[0]))
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      } else {
+        return sql
+          .insert({
+            email: email,
+            password: password,
+            date: new Date(),
+          })
+          .into('failed')
+          .then((user) => res.json(user[0]))
+          .catch((err) => {
+            res.status(400).json(err);
+          });
+      }
     })
-    .then((data) => res.status(200).json('Successful'))
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+    .catch((err) => res.status(400).json('Wrong Credentials'));
+});
+
+app.post('/register', (req, res) => {
+  const { email, password, name } = req.body;
+  const hash = bcrypt.hashSync(password, 10);
+
+  sql.transaction((trx) => {
+    trx
+      .insert({
+        password: hash,
+        email: email,
+      })
+      .into('signin')
+      .then(
+        sql('users')
+          .insert({
+            email: email,
+            name: name,
+          })
+          .then((data) => res.status(200).json('Successful'))
+          .catch((err) => {
+            res.status(400).json(err);
+          })
+      )
+      .then(trx.commit)
+      .catch(trx.rollback);
+  });
 });
 
 app.get('/users', async (req, res) => {
-  const users = await database.select('*').from('users');
+  const users = await sql.select('*').from('users');
   res.status(200).send(users);
 });
 
